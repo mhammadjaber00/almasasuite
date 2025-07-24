17447e817654d26ce8f87bf4caae42790973fbf9
Mohammad Jaber <mohamad.jaber@neo.ae>
2025-07-25 00:28:38 +0300

    Enable Dokka plugin in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
 
-
 alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka)
     alias(libs.plugins.sqldelight) apply false
     alias(libs.plugins.ktor) apply false
 }
